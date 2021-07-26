bc2adfe342ff3c71b329ef6765a276e47b3f87c7
Htutu87 <artur_amaral@poli.ufrj.br>
2021-07-26 17:09:37 -0300

    Separação dos arquivos. Organização de código.

--- t1/disparador.c
+++ t1/disparador.c
@@ -3,43 +3,35 @@
 // Autor: Artur Amaral | DRE: 119057968
 // Descrição: Programa principal.
 
-
-#include <stdio.h>
 #include <unistd.h>
 #include <signal.h>
-#include <stdlib.h>
+#include <stdbool.h>
+#include "tarefas.h"
+
 
 bool esperaSinal = false;
-
-int tarefaUm(){
-    printf("Inside tarefaUm() function.\n");
-    return 0;
-}
-
-int tarefaDois(){
-    printf("Inside tarefaDois() function.\n");
-    return 0;
-}
 
 void signalHandler(int numeroSinal)
 {
 
     if (numeroSinal == SIGUSR1){
         // Executa tarefaUm();
-        printf("received SIGUSR1\n");
+        printf("Recebido SIGUSR1.\n");
         tarefaUm();
     }
     
 
     else if (numeroSinal == SIGUSR2){
         // Executa tarefaDois();
-        printf("received SIGUSR2\n");
+        printf("Recebido SIGUSR2.\n");
         tarefaDois();
     }
     
 
-    else if (numeroSinal == SIGTERM)
+    else if (numeroSinal == SIGTERM){
+        printf("Recebido SIGTERM.\n");
         esperaSinal = !esperaSinal;
+    }
   
 }
 
@@ -56,7 +48,7 @@ int main(){
 
     
     while (!esperaSinal){
-        printf("Inside main function\n");
+        printf("Esperando sinal...\n");
         sleep(1);
     }
 
---
+++ t1/tarefas.h
@@ -0,0 +1,17 @@
+// EEL770 - Sistemas Operacionais
+// UFRJ - 2021.1 - prof: Rodrigo Couto
+// Autor: Artur Amaral | DRE: 119057968
+// Descrição: Tarefas
+
+#ifndef TAREFAS_H
+#define TAREFAS_H
+
+#include <stdio.h>
+
+int tarefaUm();
+int tarefaDois();
+
+#endif
+
+
+
